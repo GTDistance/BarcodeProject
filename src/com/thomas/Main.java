8dfdae85e57df4ca4f24921cb2a0ee11c27e665a
thomas <thomas@github.com>
2018-08-29 09:27:35 +0800

    修改二维码字符串

--- src/com/thomas/Main.java
+++ src/com/thomas/Main.java
@@ -9,19 +9,24 @@ public class Main {
 //        hlht://00000000020000002790001001.328096210/
 
         String bootFilePath = "/Users/thomas/Documents/玖行/合肥项目/桩的二维码/";
-        String fileName = "特来电二维码";
+//        String fileName = "特来电二维码";
+        String fileName = "盛宏联营";
         String filePath = bootFilePath+fileName;
         String sourceFilePath = filePath;
         String zipFilePath = filePath;
 
+//        //桩编码和枪口号
+//        String[] pileGunCodes = {
+//                "0015010000020001001",
+//                "0015010000020002001",
+//                "0015010000020003001",
+//                "0010010000020004001",
+//                "0010010000020005001",
+//                "0010010000020006001",
+//        };
         //桩编码和枪口号
         String[] pileGunCodes = {
-                "0015010000020001001",
-                "0015010000020002001",
-                "0015010000020003001",
-                "0010010000020004001",
-                "0010010000020005001",
-                "0010010000020006001",
+                "0010010000010001001"
         };
         String[] texts =new String[pileGunCodes.length];
         for(int i = 0; i<pileGunCodes.length;i++){
